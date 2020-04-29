caea3a6d28dc7cab89b485c1a6cd06adbe51db27
Javier Andres Caicedo Lopez <jcaicedo@homecenter.co>
2020-04-28 21:41:27 -0500

    confirmar

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -4,4 +4,13 @@ import {Component} from '@angular/core'
     selector:'app-body',
     templateUrl:'./body.component.html'
 })
-export class BodyComponent{}
+export class BodyComponent{
+    mostrar = true;
+
+    frase:any ={
+       mensaje :'lorem ipsum dolor sit amet',
+       autor   : 'Unknown'
+    }
+
+    personajes:string[]=['Spiderman','Venom','Octopus']
+}
